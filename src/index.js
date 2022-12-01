325f7ebf8da90f20b4322dda29afbbdc07463a11
Stas-0k <brickbybrick15@gmail.com>
2022-12-01 23:25:36 +0200

    commit

--- src/index.js
+++ src/index.js
@@ -4,7 +4,8 @@ const input = document.querySelector("input")
 input.addEventListener('input', onInput)
 
 
-function onInput() { 
+function onInput(e) { 
     const inputSearch = input.value;
-    fetch("https://pixabay.com/api/?key=31763935-81785faf577ab21332a77c6ed&image_type=photo&orientation=horizontal&safesearch=true&q=${inputSearch}")
+    
+    return fetch(`https://pixabay.com/api/?key=31763935-81785faf577ab21332a77c6ed&q=${inputSearch}&image_type=photo&orientation=horizontal&safesearch=true`)
 }
