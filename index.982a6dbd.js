5586a499f36f8222db7c29b3b0e0f0cdee2c79fe
Stas-0k <brickbybrick15@gmail.com>
2022-12-02 12:46:02 +0000

    Deploying to gh-pages from @ Stas-0k/goit-js-hw-11@d3ae84bbe775fb46b018c08ca5a892e443fc49df 🚀

--- index.982a6dbd.js
+++
@@ -1,2 +0,0 @@
-const t=document.querySelector("input");t.addEventListener("input",(function(e){const a=t.value;return fetch(`https://pixabay.com/api/?key=31763935-81785faf577ab21332a77c6ed&q=${a}&image_type=photo&orientation=horizontal&safesearch=true`)}));
-//# sourceMappingURL=index.982a6dbd.js.map
--- index.b38b4ff2.js
+++
@@ -1,2 +0,0 @@
-!function(){var t=document.querySelector("input");t.addEventListener("input",(function(e){var a=t.value;return fetch("https://pixabay.com/api/?key=31763935-81785faf577ab21332a77c6ed&q=".concat(a,"&image_type=photo&orientation=horizontal&safesearch=true"))}))}();
-//# sourceMappingURL=index.b38b4ff2.js.map
