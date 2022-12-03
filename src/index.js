14eb3658ba58bd54d0e97f8349e99c0a51bb239d
Stas-0k <brickbybrick15@gmail.com>
2022-12-03 19:41:30 +0200

    commit
    commit

--- src/index.js
+++ src/index.js
@@ -1,61 +1,65 @@
 import axios from 'axios';
 import Notiflix from 'notiflix';
+import SimpleLightbox from "simplelightbox";
+import "simplelightbox/dist/simple-lightbox.min.css";
 const input = document.querySelector('input');
 const form = document.querySelector('.search-form');
-const gallery = document.querySelector('.gallery')
-const loadButton = document.querySelector('.load-more')
+const gallery = document.querySelector('.gallery');
+const loadButton = document.querySelector('.load-more');
 
 const URL = 'https://pixabay.com/api/';
-  const params = {
-    key: '31763935-81785faf577ab21332a77c6ed',
-    q:'',
-    image_type: 'photo',
-    orientation: 'horizontal',
-    safesearch: true,
-    page: 1,
-    per_page: 40,
+const params = {
+  key: '31763935-81785faf577ab21332a77c6ed',
+  q: '',
+  image_type: 'photo',
+  orientation: 'horizontal',
+  safesearch: true,
+  page: 1,
+  per_page: 40,
 };
-  
+let hits = 40;
+
 form.addEventListener('submit', onInput);
 
 async function onInput(evt) {
   gallery.innerHTML = '';
-    
+  hits = 40;
   evt.preventDefault();
   const searchItem = input.value;
-  
   params.q = searchItem;
-
-  const result = await axios(URL, { params } )
-    
-
+  const result = await axios(URL, { params });
+ 
+  
   if (!result.data.hits.length) {
-    Notiflix.Notify.failure('Sorry, there are no images matching your search query. Please try again.');
+    Notiflix.Notify.failure(
+      'Sorry, there are no images matching your search query. Please try again.'
+    );
   }
+  
   renderPhotos(result.data.hits);
-
-   
 }
 
-async function loadFunc() {
-  params.page += 1;
-    const result = await axios(URL, { params })
-    console.log(result)
-    
-    renderPhotos(result.data.hits);
-  }
-
-   loadButton.addEventListener('click', loadFunc)
 
   
+async function loadFunc() {
+  params.page += 1;
+  const result = await axios(URL, { params });
+  hits += result.data.hits.length
+  renderPhotos(result.data.hits);
+  Notiflix.Notify.info(`Hooray! We found ${hits} images.`);
+  if (hits === result.data.totalHits) { 
+    loadButton.style.visibility = 'hidden';
+    Notiflix.Notify.info("We're sorry, but you've reached the end of search results.");
+  }
+}
+loadButton.addEventListener('click', loadFunc);
 
- 
-
-
-
-function renderPhotos(data) { 
-    const markup = data.map(item => `<div class="photo-card">
-  <img src="${item.webformatURL}" alt="${item.tags}" loading="lazy" class="image"/>
+function renderPhotos(data) {
+  
+  const markup = data.map(
+    item => `<div class="photo-card">
+    <a class="item-link" href=${item.largeImageURL}>
+  <img src="${item.webformatURL}" alt="${item.tags}" loading="lazy" class="image"/></a>
   <div class="info">
     <p class="info-item">
       <b>Likes: </b>${item.likes}
@@ -70,7 +74,11 @@ function renderPhotos(data) {
       <b>Downloads: </b>${item.downloads}
     </p>
   </div>
-</div>`)
-    gallery.insertAdjacentHTML('beforeend', markup)    
-    loadButton.style.visibility = 'visible';
+  
+</div>`
+  );
+  
+  gallery.insertAdjacentHTML('beforeend', markup);
+  loadButton.style.visibility = 'visible';
+  let gallerySimple = new SimpleLightbox('.item-link', { captionsData: "alt", captionDelay: 250 })
 }
