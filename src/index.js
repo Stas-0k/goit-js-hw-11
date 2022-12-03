11b25190efbbed5c099cf5abfd9bc9d0f1f23931
Stas-0k <brickbybrick15@gmail.com>
2022-12-03 17:28:04 +0200

    commit
    commit

--- src/index.js
+++ src/index.js
@@ -5,42 +5,55 @@ const form = document.querySelector('.search-form');
 const gallery = document.querySelector('.gallery')
 const loadButton = document.querySelector('.load-more')
 
+const URL = 'https://pixabay.com/api/';
+  const params = {
+    key: '31763935-81785faf577ab21332a77c6ed',
+    q:'',
+    image_type: 'photo',
+    orientation: 'horizontal',
+    safesearch: true,
+    page: 1,
+    per_page: 40,
+};
+  
 form.addEventListener('submit', onInput);
 
 async function onInput(evt) {
-    gallery.innerHTML = '';
+  gallery.innerHTML = '';
     
-    evt.preventDefault();
-    const searchItem = input.value;
-    const URL = 'https://pixabay.com/api/';
-    const params = {
-        key: '31763935-81785faf577ab21332a77c6ed',
-        q: searchItem,
-        image_type: 'photo',
-        orientation: 'horizontal',
-        safesearch: true,
-        page: 1,
-        per_page: 40, 
-    };
+  evt.preventDefault();
+  const searchItem = input.value;
+  
+  params.q = searchItem;
 
-    const result = await axios(URL, { params })
+  const result = await axios(URL, { params } )
     
-    console.log(params.page)
-    if (!result.data.hits.length) { 
-        Notiflix.Notify.failure('Sorry, there are no images matching your search query. Please try again.');
-    }
-    renderPhotos(result.data.hits);
-    loadButton.addEventListener('click', loadFunc)
 
-    function loadFunc() { 
-        params.page += 1;
-        console.log(params.page)
-        renderPhotos(result.data.hits);
-    }
+  if (!result.data.hits.length) {
+    Notiflix.Notify.failure('Sorry, there are no images matching your search query. Please try again.');
+  }
+  renderPhotos(result.data.hits);
+
+   
 }
 
+async function loadFunc() {
+  params.page += 1;
+    const result = await axios(URL, { params })
+    console.log(result)
+    
+    renderPhotos(result.data.hits);
+  }
+
+   loadButton.addEventListener('click', loadFunc)
+
+  
+
+ 
+
+
+
 function renderPhotos(data) { 
-    console.log(data)
     const markup = data.map(item => `<div class="photo-card">
   <img src="${item.webformatURL}" alt="${item.tags}" loading="lazy" class="image"/>
   <div class="info">
@@ -61,4 +74,3 @@ function renderPhotos(data) {
     gallery.insertAdjacentHTML('beforeend', markup)    
     loadButton.style.visibility = 'visible';
 }
-
